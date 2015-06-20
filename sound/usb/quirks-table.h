42181628a0f840dc643ba07172bfff86fa858fdf
friedrich420 <kelpidorou@yahoo.com>
2015-06-20 13:56:53 +0300

    Linux 3.4.108
    Conflicts:
    	drivers/usb/host/xhci-pci.c
    	drivers/usb/host/xhci.h
    	fs/exec.c
    	kernel/softirq.c
    	sound/usb/quirks-table.h
    
    Conflicts:
    	fs/exec.c

--- sound/usb/quirks-table.h
+++ sound/usb/quirks-table.h
@@ -1969,6 +1969,10 @@ YAMAHA_DEVICE(0x7010, "UB99"),
 	.driver_info = (unsigned long) & (const struct snd_usb_audio_quirk) {
 		/* .vendor_name = "BOSS", */
 		/* .product_name = "GT-100", */
+	USB_DEVICE(0x0582, 0x0159),
+	.driver_info = (unsigned long) & (const struct snd_usb_audio_quirk) {
+		/* .vendor_name = "Roland", */
+		/* .product_name = "UA-22", */
 		.ifnum = QUIRK_ANY_INTERFACE,
 		.type = QUIRK_COMPOSITE,
 		.data = (const struct snd_usb_audio_quirk[]) {
@@ -1986,6 +1990,19 @@ YAMAHA_DEVICE(0x7010, "UB99"),
 				.data = & (const struct snd_usb_midi_endpoint_info) {
 					.out_cables = 0x0001,
 					.in_cables  = 0x0001
+				.ifnum = 0,
+				.type = QUIRK_AUDIO_STANDARD_INTERFACE
+			},
+			{
+				.ifnum = 1,
+				.type = QUIRK_AUDIO_STANDARD_INTERFACE
+			},
+			{
+				.ifnum = 2,
+				.type = QUIRK_MIDI_FIXED_ENDPOINT,
+				.data = & (const struct snd_usb_midi_endpoint_info) {
+					.out_cables = 0x0001,
+					.in_cables = 0x0001
 				}
 			},
 			{
@@ -2005,7 +2022,6 @@ YAMAHA_DEVICE(0x7010, "UB99"),
 		.type = QUIRK_AUTODETECT
 	}
 },
-
 /* Guillemot devices */
 {
 	/*
