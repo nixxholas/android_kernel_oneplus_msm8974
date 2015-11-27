bdee7147e1c6a1018f21d241deb020e8308499e5
Varun <date.varun123@gmail.com>
2015-11-27 13:31:00 +0530

    UPDATE 2.7

--- build.sh
+++ build.sh
@@ -16,12 +16,12 @@ device="bacon"
 
 # Kernel Details
 BASE_RR_VER="RR"
-VER="V2.5"
+VER="V2.7"
 RR_VER="$BASE_RR_VER$VER"
 
 # Vars
 export LOCALVERSION="-$RR_VER-$(date +%Y%m%d)"
-export CROSS_COMPILE="/home/varun/RR/prebuilts/gcc/linux-x86/arm/arm-eabi-5.2/bin/arm-eabi-"
+export CROSS_COMPILE="/home/varun/rr2/prebuilts/gcc/linux-x86/arm/arm-eabi-5.2/bin/arm-eabi-"
 export ARCH=arm
 export SUBARCH=arm
 export KBUILD_BUILD_HOST=`hostname`
